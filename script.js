3250126953bb4d98b4b252f2da27e1b76416841e
Neenhila <83518175+neenhila@users.noreply.github.com>
2022-12-17 06:06:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+tsParticles.load("tsparticles", {
+  particles: {
+    number: {
+      value: 300
+    },
+    move: {
+      enable: true
+    },
+    shape: {
+      type: "star"
+    },
+    animation: {
+      enable: true,
+      speed: 20,
+      minimumValue: 0.1,
+      sync: false
+    },
+    links: {
+      enable: true,
+      distance: 300,
+      color: "#696969",
+      opacity: 0.2,
+      width: 1
+    }
+  },
+  interactivity: {
+    detectsOn: "canvas",
+    events: {
+      onHover: {
+        enable: true,
+        mode: "repulse"
+      },
+
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        links: {
+          opacity: 1
+        }
+      },
+      repulse: {
+        distance: 80
+      },
+      push: {
+        quantity: 4
+      },
+      remove: {
+        quantity: 2
+      }
+    }
+  },
+  detectRetina: true,
+})
